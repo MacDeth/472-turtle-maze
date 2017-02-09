d6823d92a6ba30db0c89f5e7f580744dc975a070
MacDeth <jam.dangelo@gmail.com>
2017-02-09 15:16:36 -0500

    fsm for finding the end of the maze outline?

---
+++ fsm_endfind.lua
@@ -0,0 +1,16 @@
+local x, y, z = gps.locate(5)
+
+local function FindEnd()
+  local end_found = false
+
+  while not end_found do
+    -- iterate movement in search algo
+    -- check if end position -> change end_found to true
+  end
+end
+
+local function IsEnd()
+  -- if below block is red wool then
+  -- return true
+  return false; -- placeholder
+end
