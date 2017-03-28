7f538b9c8cd4c4fdeabe32a79d4588157f00607d
Josh Simons <Josh Simons>
2017-03-28 12:56:28 -0400

    Added display for monitors

---
+++ display.lua
@@ -0,0 +1,30 @@
+-- open rednet
+rednet.open("left")
+
+--listen for state and map updates
+--listen for exit state to end infinite display loop
+while(true) do
+    senderID, state = rednet.receive()
+    if state == "exit" then 
+        break 
+    end
+    io.write("Current state: ", state)
+    io.write("\n")
+    --listen for the row and column length
+    senderID, rowsString = rednet.receive()
+    senderID, columnsString = rednet.receive()
+    rows = tonumber(rowsString)
+    columns = tonumber(columnsString)
+    --listen for string of map and print it in an array format
+    senderID, mapString = rednet.receive()
+    count = 1
+    io.write("Current map known by the AI robot: ")
+    io.write("\n")
+    for i = 1, rows do
+        for j = 1, columns do
+            io.write(string.sub(mapString, count, count))
+            count = count + 2
+        end
+        io.write("\n")
+    end
+end
