9055cc47d1b25c23804bf00f8a95121be7002162
Jason Schafer <jason59549@gmail.com>
2017-03-30 20:41:47 -0400

    Completed Djiktstra Search Algorithm
    This is sufficient for a demoing and shows us it can work for minecraft!

--- search_djikstra.lua
+++ search_djikstra.lua
@@ -2,17 +2,20 @@
 maze = {
     {0,  0,  0,  0,  0,  0,  0},
     {0,  1,  0,  7,  8,  9,  0},
-    {0,  2,  0,  6,  0, 10,  0},
-    {0,  3,  4,  5,  0, 11,  18},
-    {0,  8,  0,  0,  0, 12,  0},
-    {0, 17, 16, 15, 14, 13,  0},
-    {0,  0,  0,  0,  0,  0,  0}
+    {0,  2,  0,  6,  0,  10,  0},
+    {0,  3,  4,  5,  0,  11,  0},
+    {0,  18, 0,  0,  0,  12,  0},
+    {0,  17, 16, 15, 14, 13,  0},
+    {0,  0,  0,  0,  0,  19,  0}
 }
 N = 7 --Width of Maze
 M = 7 --Height of Maze
 
 mt = {}
-numNodes = 18 --Each Open Block is assigned a Number when found. Goal Node is highest
+numNodes = 19 --Each Open Block is assigned a Number when found. Goal Node is highest
+
+source = 1
+target = 19
 
 function fillMatrix()
     for i=1,numNodes do
@@ -47,27 +50,49 @@ end
 
 function Djikstra()
 
-	--Initialize
+	--Initialization
 	Q = {}
+	Qsize = 0
 	dist = {}
 	prev = {}
 
 	for v=1, numNodes do
-		if v ~= source then
-			dist[v] = math.huge
-			prev[v] = 1 --Each Square has equal distance of 1.
-			table.insert(Q, v) --Add v to set Q
-		end
+			dist[v] = math.huge --Unknown Distance from source to v
+			prev[v] = nil --Previous node in optimal path from source
+			Q[v] = true --All nodes initially in Q (unvisited nodes)
+			Qsize = Qsize + 1
 	end
 
-	dist[source] = 0
+	dist[source] = 0 --Distance from source to source
 
-	while(getn(visited) > 0) --Iterate While Q not Empty
-		for(u=1, numNodes) do --Iterate through set
-			Q[u] = nil; --Remove u from set Q
-			for(v=1, numNodes) do
-				if [u][v] = 1 then --For Each Neighbor
-					
+	while Qsize > 0 do --Iterate While Q not Empty
+		u = 0 --Instantiate u variable
+
+		--Node with least distance selected first
+		mindist = math.huge
+		for k, v in pairs(Q) do --Iterate through set
+			if v ~= nil then
+				if dist[k] <= mindist then
+						u = k
+						mindist = dist[k]
+				end
+			end
+		end
+
+		Q[u] = nil; --Remove u from set Q
+		Qsize = Qsize - 1
+
+		if (u == target) then
+			break
+		end
+
+		for v in pairs(Q) do
+			if mt[u][v] == 1 then --For Each Neighbor
+				alt = dist[u] + 1 --Length from U to V is always 1
+				if alt < dist[v] then
+					dist[v] = alt
+					prev[v] = u
+				end
 			end
 		end
 	end
@@ -75,22 +100,39 @@ end
 
 fillMatrix()
 adjacencyMatrix()
-dikstraSearch()
-
-
 
 print("Original Maze:")
 for i=1, N do
    for j=1, M do
-       io.write(maze[i][j])
+       io.write(string.format("%3d", maze[i][j]))
    end
    print()
 end
 
-print("Adjacency Matrix:")
+print("\nAdjacency Matrix:")
 for i=1, numNodes do
    for j=1, numNodes do
-       io.write(mt[i][j])
+       io.write(string.format("%2d", mt[i][j]))
    end
    print()
+end
+
+Djikstra()
+
+S = {}
+u = target
+print("\nDjikstra Path:")
+
+--Construct Path To Pring
+while prev[u] ~= nil do
+	table.insert(S, 1, u)
+	u = prev[u]
+end
+table.insert(S, 1, u) --Get final Num
+
+for i = 1, #S do
+	io.write(S[i])
+	if i ~= #S then
+		io.write("->")
+	end
 end
