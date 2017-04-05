cb9d2c5ecadd759d99b029d0c8d515c74fdfb1f7
MacDeth <jam.dangelo@gmail.com>
2017-04-05 18:02:42 -0400

    beautified

--- mazeGen.lua
+++ mazeGen.lua
@@ -3,7 +3,7 @@
 --length should be odd
 
 length = 15
-map = {} --final grid 
+map = {} --final grid
 
 cellGrid = {}
 gridLen = (length-1)/2
@@ -33,81 +33,81 @@ math.randomseed(os.time())
 while generating == 1 do
   n = math.random(0,3)
   cellGrid[current["x"]][current["y"]]["visited"] = 1
-  
+
   if n == 0 then
     -- move left if possible
     if current["x"] ~= 1 and cellGrid[current["x"]-1][current["y"]]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["l"] = 0
-        cellGrid[current["x"]-1][current["y"]]["r"] = 0
-        cellGrid[current["x"]-1][current["y"]]["prevX"] = current["x"]
-        cellGrid[current["x"]-1][current["y"]]["prevY"] = current["y"]
-        current["x"] = current["x"] - 1
-    -- move right if possible
+      cellGrid[current["x"]][current["y"]]["l"] = 0
+      cellGrid[current["x"]-1][current["y"]]["r"] = 0
+      cellGrid[current["x"]-1][current["y"]]["prevX"] = current["x"]
+      cellGrid[current["x"]-1][current["y"]]["prevY"] = current["y"]
+      current["x"] = current["x"] - 1
+      -- move right if possible
     elseif current["x"] ~= gridLen and cellGrid[current["x"]+1][current["y"]]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["r"] = 0
-        cellGrid[current["x"]+1][current["y"]]["l"] = 0
-        cellGrid[current["x"]+1][current["y"]]["prevX"] = current["x"]
-        cellGrid[current["x"]+1][current["y"]]["prevY"] = current["y"]
-        current["x"] = current["x"] + 1
-    -- move up if possible
-    elseif current["y"] ~= 1 and cellGrid[current["x"]][current["y"]-1]["visited"] == 0 then    
-        cellGrid[current["x"]][current["y"]]["u"] = 0
-        cellGrid[current["x"]][current["y"]-1]["d"] = 0
-        cellGrid[current["x"]][current["y"]-1]["prevX"] = current["x"]
-        cellGrid[current["x"]][current["y"]-1]["prevY"] = current["y"]
-        current["y"] = current["y"] - 1
-    -- move down if possible
+      cellGrid[current["x"]][current["y"]]["r"] = 0
+      cellGrid[current["x"]+1][current["y"]]["l"] = 0
+      cellGrid[current["x"]+1][current["y"]]["prevX"] = current["x"]
+      cellGrid[current["x"]+1][current["y"]]["prevY"] = current["y"]
+      current["x"] = current["x"] + 1
+      -- move up if possible
+    elseif current["y"] ~= 1 and cellGrid[current["x"]][current["y"]-1]["visited"] == 0 then
+      cellGrid[current["x"]][current["y"]]["u"] = 0
+      cellGrid[current["x"]][current["y"]-1]["d"] = 0
+      cellGrid[current["x"]][current["y"]-1]["prevX"] = current["x"]
+      cellGrid[current["x"]][current["y"]-1]["prevY"] = current["y"]
+      current["y"] = current["y"] - 1
+      -- move down if possible
     elseif current["y"] ~= gridLen and cellGrid[current["x"]][current["y"]+1]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["d"] = 0
-        cellGrid[current["x"]][current["y"]+1]["u"] = 0
-        cellGrid[current["x"]][current["y"]+1]["prevX"] = current["x"]
-        cellGrid[current["x"]][current["y"]+1]["prevY"] = current["y"]  
-        current["y"] = current["y"] + 1
-    -- move back
-  else
-    if cellGrid[current["x"]][current["y"]]["prevX"] == -1 then
-      generating = 0
-    elseif cellGrid[current["x"]][current["y"]]["prevY"] == -1 then
-      generating = 0
+      cellGrid[current["x"]][current["y"]]["d"] = 0
+      cellGrid[current["x"]][current["y"]+1]["u"] = 0
+      cellGrid[current["x"]][current["y"]+1]["prevX"] = current["x"]
+      cellGrid[current["x"]][current["y"]+1]["prevY"] = current["y"]
+      current["y"] = current["y"] + 1
+      -- move back
     else
-      current["x"] = cellGrid[current["x"]][current["y"]]["prevX"]
-      current["y"] = cellGrid[current["x"]][current["y"]]["prevY"]
-      if current["x"] == -1 or current["y"] == -1 then
+      if cellGrid[current["x"]][current["y"]]["prevX"] == -1 then
         generating = 0
+      elseif cellGrid[current["x"]][current["y"]]["prevY"] == -1 then
+        generating = 0
+      else
+        current["x"] = cellGrid[current["x"]][current["y"]]["prevX"]
+        current["y"] = cellGrid[current["x"]][current["y"]]["prevY"]
+        if current["x"] == -1 or current["y"] == -1 then
+          generating = 0
+        end
       end
     end
-  end
-  
+
   elseif n == 1 then
-     -- move down if possible
+    -- move down if possible
     if current["y"] ~= gridLen and cellGrid[current["x"]][current["y"]+1]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["d"] = 0
-        cellGrid[current["x"]][current["y"]+1]["u"] = 0
-        cellGrid[current["x"]][current["y"]+1]["prevX"] = current["x"]
-        cellGrid[current["x"]][current["y"]+1]["prevY"] = current["y"]      
-        current["y"] = current["y"] + 1
-    -- move up if possible
+      cellGrid[current["x"]][current["y"]]["d"] = 0
+      cellGrid[current["x"]][current["y"]+1]["u"] = 0
+      cellGrid[current["x"]][current["y"]+1]["prevX"] = current["x"]
+      cellGrid[current["x"]][current["y"]+1]["prevY"] = current["y"]
+      current["y"] = current["y"] + 1
+      -- move up if possible
     elseif current["y"] ~= 1 and cellGrid[current["x"]][current["y"]-1]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["u"] = 0
-        cellGrid[current["x"]][current["y"]-1]["d"] = 0
-        cellGrid[current["x"]][current["y"]-1]["prevX"] = current["x"]
-        cellGrid[current["x"]][current["y"]-1]["prevY"] = current["y"]
-        current["y"] = current["y"] - 1
-    -- move right if possible
+      cellGrid[current["x"]][current["y"]]["u"] = 0
+      cellGrid[current["x"]][current["y"]-1]["d"] = 0
+      cellGrid[current["x"]][current["y"]-1]["prevX"] = current["x"]
+      cellGrid[current["x"]][current["y"]-1]["prevY"] = current["y"]
+      current["y"] = current["y"] - 1
+      -- move right if possible
     elseif current["x"] ~= gridLen and cellGrid[current["x"]+1][current["y"]]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["r"] = 0
-        cellGrid[current["x"]+1][current["y"]]["l"] = 0
-        cellGrid[current["x"]+1][current["y"]]["prevX"] = current["x"]
-        cellGrid[current["x"]+1][current["y"]]["prevY"] = current["y"]
-        current["x"] = current["x"] + 1
-    -- move left if possible
+      cellGrid[current["x"]][current["y"]]["r"] = 0
+      cellGrid[current["x"]+1][current["y"]]["l"] = 0
+      cellGrid[current["x"]+1][current["y"]]["prevX"] = current["x"]
+      cellGrid[current["x"]+1][current["y"]]["prevY"] = current["y"]
+      current["x"] = current["x"] + 1
+      -- move left if possible
     elseif current["x"] ~= 1 and cellGrid[current["x"]-1][current["y"]]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["l"] = 0
-        cellGrid[current["x"]-1][current["y"]]["r"] = 0
-        cellGrid[current["x"]-1][current["y"]]["prevX"] = current["x"]
-        cellGrid[current["x"]-1][current["y"]]["prevY"] = current["y"]
-        current["x"] = current["x"] - 1
-    -- move back
+      cellGrid[current["x"]][current["y"]]["l"] = 0
+      cellGrid[current["x"]-1][current["y"]]["r"] = 0
+      cellGrid[current["x"]-1][current["y"]]["prevX"] = current["x"]
+      cellGrid[current["x"]-1][current["y"]]["prevY"] = current["y"]
+      current["x"] = current["x"] - 1
+      -- move back
     else
       if cellGrid[current["x"]][current["y"]]["prevX"] == -1 then
         generating = 0
@@ -124,109 +124,107 @@ while generating == 1 do
   elseif n == 2 then
     -- move right if possible
     if current["x"] ~= gridLen and cellGrid[current["x"]+1][current["y"]]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["r"] = 0
-        cellGrid[current["x"]+1][current["y"]]["l"] = 0
-        cellGrid[current["x"]+1][current["y"]]["prevX"] = current["x"]
-        cellGrid[current["x"]+1][current["y"]]["prevY"] = current["y"]
-        current["x"] = current["x"] + 1
-    -- move left if possible
+      cellGrid[current["x"]][current["y"]]["r"] = 0
+      cellGrid[current["x"]+1][current["y"]]["l"] = 0
+      cellGrid[current["x"]+1][current["y"]]["prevX"] = current["x"]
+      cellGrid[current["x"]+1][current["y"]]["prevY"] = current["y"]
+      current["x"] = current["x"] + 1
+      -- move left if possible
     elseif current["x"] ~= 1 and cellGrid[current["x"]-1][current["y"]]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["l"] = 0
-        cellGrid[current["x"]-1][current["y"]]["r"] = 0
-        cellGrid[current["x"]-1][current["y"]]["prevX"] = current["x"]
-        cellGrid[current["x"]-1][current["y"]]["prevY"] = current["y"]
-        current["x"] = current["x"] - 1
-    -- move down if possible
+      cellGrid[current["x"]][current["y"]]["l"] = 0
+      cellGrid[current["x"]-1][current["y"]]["r"] = 0
+      cellGrid[current["x"]-1][current["y"]]["prevX"] = current["x"]
+      cellGrid[current["x"]-1][current["y"]]["prevY"] = current["y"]
+      current["x"] = current["x"] - 1
+      -- move down if possible
     elseif current["y"] ~= gridLen and cellGrid[current["x"]][current["y"]+1]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["d"] = 0
-        cellGrid[current["x"]][current["y"]+1]["u"] = 0
-        cellGrid[current["x"]][current["y"]+1]["prevX"] = current["x"]
-        cellGrid[current["x"]][current["y"]+1]["prevY"] = current["y"]  
-        current["y"] = current["y"] + 1
-    -- move up if possible
+      cellGrid[current["x"]][current["y"]]["d"] = 0
+      cellGrid[current["x"]][current["y"]+1]["u"] = 0
+      cellGrid[current["x"]][current["y"]+1]["prevX"] = current["x"]
+      cellGrid[current["x"]][current["y"]+1]["prevY"] = current["y"]
+      current["y"] = current["y"] + 1
+      -- move up if possible
     elseif current["y"] ~= 1 and cellGrid[current["x"]][current["y"]-1]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["u"] = 0
-        cellGrid[current["x"]][current["y"]-1]["d"] = 0
-        cellGrid[current["x"]][current["y"]-1]["prevX"] = current["x"]
-        cellGrid[current["x"]][current["y"]-1]["prevY"] = current["y"]
-        current["y"] = current["y"] - 1
-    -- move back
-  else
-    if cellGrid[current["x"]][current["y"]]["prevX"] == -1 then
-      generating = 0
-    elseif cellGrid[current["x"]][current["y"]]["prevY"] == -1 then
-      generating = 0
+      cellGrid[current["x"]][current["y"]]["u"] = 0
+      cellGrid[current["x"]][current["y"]-1]["d"] = 0
+      cellGrid[current["x"]][current["y"]-1]["prevX"] = current["x"]
+      cellGrid[current["x"]][current["y"]-1]["prevY"] = current["y"]
+      current["y"] = current["y"] - 1
+      -- move back
     else
-      current["x"] = cellGrid[current["x"]][current["y"]]["prevX"]
-      current["y"] = cellGrid[current["x"]][current["y"]]["prevY"]
-      if current["x"] == -1 or current["y"] == -1 then
+      if cellGrid[current["x"]][current["y"]]["prevX"] == -1 then
         generating = 0
+      elseif cellGrid[current["x"]][current["y"]]["prevY"] == -1 then
+        generating = 0
+      else
+        current["x"] = cellGrid[current["x"]][current["y"]]["prevX"]
+        current["y"] = cellGrid[current["x"]][current["y"]]["prevY"]
+        if current["x"] == -1 or current["y"] == -1 then
+          generating = 0
+        end
       end
+
     end
-   
-  end
-  
+
   elseif n == 3 then
     -- move up if possible
     if current["y"] ~= 1 and cellGrid[current["x"]][current["y"]-1]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["u"] = 0
-        cellGrid[current["x"]][current["y"]-1]["d"] = 0
-        cellGrid[current["x"]][current["y"]-1]["prevX"] = current["x"]
-        cellGrid[current["x"]][current["y"]-1]["prevY"] = current["y"]
-        current["y"] = current["y"] - 1
-    -- move down if possible
+      cellGrid[current["x"]][current["y"]]["u"] = 0
+      cellGrid[current["x"]][current["y"]-1]["d"] = 0
+      cellGrid[current["x"]][current["y"]-1]["prevX"] = current["x"]
+      cellGrid[current["x"]][current["y"]-1]["prevY"] = current["y"]
+      current["y"] = current["y"] - 1
+      -- move down if possible
     elseif current["y"] ~= gridLen and cellGrid[current["x"]][current["y"]+1]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["d"] = 0
-        cellGrid[current["x"]][current["y"]+1]["u"] = 0
-        cellGrid[current["x"]][current["y"]+1]["prevX"] = current["x"]
-        cellGrid[current["x"]][current["y"]+1]["prevY"] = current["y"]     
-        current["y"] = current["y"] + 1
-    -- move left if possible
+      cellGrid[current["x"]][current["y"]]["d"] = 0
+      cellGrid[current["x"]][current["y"]+1]["u"] = 0
+      cellGrid[current["x"]][current["y"]+1]["prevX"] = current["x"]
+      cellGrid[current["x"]][current["y"]+1]["prevY"] = current["y"]
+      current["y"] = current["y"] + 1
+      -- move left if possible
     elseif current["x"] ~= 1 and cellGrid[current["x"]-1][current["y"]]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["l"] = 0
-        cellGrid[current["x"]-1][current["y"]]["r"] = 0
-        cellGrid[current["x"]-1][current["y"]]["prevX"] = current["x"]
-        cellGrid[current["x"]-1][current["y"]]["prevY"] = current["y"]
-        current["x"] = current["x"] - 1
-    -- move right if possible
+      cellGrid[current["x"]][current["y"]]["l"] = 0
+      cellGrid[current["x"]-1][current["y"]]["r"] = 0
+      cellGrid[current["x"]-1][current["y"]]["prevX"] = current["x"]
+      cellGrid[current["x"]-1][current["y"]]["prevY"] = current["y"]
+      current["x"] = current["x"] - 1
+      -- move right if possible
     elseif current["x"] ~= gridLen and cellGrid[current["x"]+1][current["y"]]["visited"] == 0 then
-        cellGrid[current["x"]][current["y"]]["r"] = 0
-        cellGrid[current["x"]+1][current["y"]]["l"] = 0
-        cellGrid[current["x"]+1][current["y"]]["prevX"] = current["x"]
-        cellGrid[current["x"]+1][current["y"]]["prevY"] = current["y"]
-        current["x"] = current["x"] + 1
-    -- move back
-  else
-    if cellGrid[current["x"]][current["y"]]["prevX"] == -1 then
-      generating = 0
-    elseif cellGrid[current["x"]][current["y"]]["prevY"] == -1 then
-      generating = 0
+      cellGrid[current["x"]][current["y"]]["r"] = 0
+      cellGrid[current["x"]+1][current["y"]]["l"] = 0
+      cellGrid[current["x"]+1][current["y"]]["prevX"] = current["x"]
+      cellGrid[current["x"]+1][current["y"]]["prevY"] = current["y"]
+      current["x"] = current["x"] + 1
+      -- move back
     else
-      current["x"] = cellGrid[current["x"]][current["y"]]["prevX"]
-      current["y"] = cellGrid[current["x"]][current["y"]]["prevY"]
-      if current["x"] == -1 or current["y"] == -1 then
+      if cellGrid[current["x"]][current["y"]]["prevX"] == -1 then
         generating = 0
+      elseif cellGrid[current["x"]][current["y"]]["prevY"] == -1 then
+        generating = 0
+      else
+        current["x"] = cellGrid[current["x"]][current["y"]]["prevX"]
+        current["y"] = cellGrid[current["x"]][current["y"]]["prevY"]
+        if current["x"] == -1 or current["y"] == -1 then
+          generating = 0
+        end
       end
     end
   end
-  
-end
 end
 
 for i = 1, gridLen do
   x = ""
   for j = 1, gridLen do
-    x= x .. "   " .. cellGrid[i][j]['l'] .. cellGrid[i][j]['d'] .. cellGrid[i][j]['r'] .. cellGrid[i][j]['u'] 
+    x= x .. " " .. cellGrid[i][j]['l'] .. cellGrid[i][j]['d'] .. cellGrid[i][j]['r'] .. cellGrid[i][j]['u']
   end
   --print(x)
 end
-
 
 gridMap = {}
 for i=1, length do
   gridMap[i] = {}
   x = ""
-  for j=1, length do 
+  for j=1, length do
     y = ""
     if i == 1 or i == length or j == 1 or j == length then
       gridMap[i][j] = 1
@@ -241,8 +239,8 @@ for i=1, length do
         gridMap[i][j] = cellGrid[(i/2)][(j-1)/2]["u"]
       end
     end
-    
-    x  = x .. gridMap[i][j]
+
+    x = x .. gridMap[i][j]
   end
   print(x)
- end
+end
